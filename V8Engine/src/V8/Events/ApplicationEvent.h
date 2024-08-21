e35d95239eaf05ffdc05ae08c6f94541bd174abc
Jeongmin94 <extraking@naver.com>
2024-08-21 11:13:14 +0900

    add EventDispatcher

--- V8Engine/src/V8/Events/ApplicationEvent.h
+++ V8Engine/src/V8/Events/ApplicationEvent.h
@@ -1,1 +1,22 @@
 #pragma once
+
+#include "V8/Events/Event.h"
+
+namespace V8
+{
+class WindowResizeEvent : public Event
+{
+public:
+    WindowResizeEvent(uint32 width, uint32 height)
+        : _width(width), _height(height)
+    {
+    }
+
+    EVENT_CLASS_TYPE(WindowResize);
+    EVENT_CLASS_CATEGORY(EventCategory::Application |
+                         EventCategory::Keyboard);
+
+private:
+    uint32 _width, _height;
+};
+} // namespace V8
--- V8Engine/src/V8/Events/Event.h
+++ V8Engine/src/V8/Events/Event.h
@@ -51,16 +51,46 @@ public:
 
     virtual EventType GetEventType() const = 0;
     virtual const char* GetName() const = 0;
-    virtual int GetCategoryFlags() const = 0;
+    virtual uint32 GetCategoryFlags() const = 0;
     virtual std::string ToString() const { return GetName(); }
 
 public:
-    bool Handled = false;
+    bool _handled = false;
 
     bool IsInCategory(EventCategory category) const
     {
         return GetCategoryFlags() & static_cast<uint32>(category);
     }
 };
+
+class EventDispatcher
+{
+public:
+    EventDispatcher(Event& event) : _event(event) {}
+
+    template <typename T>
+    using EventFn = std::function<bool>(T&);
+
+    template <typename T>
+    bool Dispatch(EventFn<T> func)
+    {
+        if (_event.GetEventType() == T::GetStaticType())
+        {
+            _event._handled = func(static_cast<T&>(_event));
+            return true;
+        }
+
+        return false;
+    }
+
+private:
+    Event& _event;
+};
+
+std::ostream& operator<<(std::ostream& out, const Event& e)
+{
+    out << e.ToString();
+    return out;
+}
 
 } // namespace V8
