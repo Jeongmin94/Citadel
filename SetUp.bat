14978196ec57108cc6a74ecad39630861e25a8e0
Jeongmin94 <extraking@naver.com>
2024-08-23 23:50:25 +0900

    edit SetUp.bat for GLFW submodule

--- SetUp.bat
+++ SetUp.bat
@@ -1,2 +1,13 @@
+@echo off
+REM GLFW premake5
+set glfw=GLFWpremake5.lua
+set glfwDestination=V8Engine\vendor\GLFW\premake5.lua
+if exist "%glfw%" (
+    copy "%glfw%" "%glfwDestination%"
+    echo move done: %glfwDestination%
+) else (
+    echo file is missing: %glfw%
+)
+
 call vendor\bin\premake\premake5.exe vs2022
 PAUSE
