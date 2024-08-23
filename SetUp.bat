6b36fd363ca1de9acb52ab1d882f2f608153bbab
jeongmin94 <extraking@naver.com>
2024-08-24 01:10:32 +0900

    edit SetUp.bat
    - check if exist premake5.lua in V8Engine\vendor\GLFW

--- SetUp.bat
+++ SetUp.bat
@@ -3,8 +3,12 @@ REM GLFW premake5
 set glfw=GLFWpremake5.lua
 set glfwDestination=V8Engine\vendor\GLFW\premake5.lua
 if exist "%glfw%" (
-    copy "%glfw%" "%glfwDestination%"
-    echo move done: %glfwDestination%
+    if not exist "%glfwDestination%" (
+        copy "%glfw%" "%glfwDestination%"
+        echo copy done: %glfwDestination%
+    ) else (
+        echo premake5.lua for GLFW script is already exist
+    )
 ) else (
     echo file is missing: %glfw%
 )
