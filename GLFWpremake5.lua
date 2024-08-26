66db80af8f47d01b06031400959378d96a1fe9f7
Jeongmin94 <extraking@naver.com>
2024-08-26 22:30:50 +0900

    fix GLFW premake5 script
    - When Compiling V8Engine, some dlls about opengl is missing.
    - Add some glfw .c, .h to fix this

--- GLFWpremake5.lua
+++ GLFWpremake5.lua
@@ -7,15 +7,27 @@ project "GLFW"
 
 	files
 	{
-		"include/GLFW/glfw3.h",
-		"include/GLFW/glfw3native.h",
-		"src/glfw_config.h",
-		"src/context.c",
-		"src/init.c",
-		"src/input.c",
-		"src/monitor.c",
-		"src/vulkan.c",
-		"src/window.c"
+        "include/GLFW/glfw3.h",
+        "include/GLFW/glfw3native.h",
+        "src/internal.h",
+        "src/platform.h",
+        "src/mappings.h",
+        "src/context.c",
+        "src/init.c",
+        "src/input.c",
+        "src/monitor.c",
+        "src/platform.c",
+        "src/vulkan.c",
+        "src/window.c",
+        "src/egl_context.c",
+        "src/osmesa_context.c",
+        "src/null_platform.h",
+        "src/null_joystick.h",
+        "src/null_init.c",
+    
+        "src/null_monitor.c",
+        "src/null_window.c",
+        "src/null_joystick.c",
 	}
 
     filter "system:windows"
@@ -25,9 +37,12 @@ project "GLFW"
         files
         {
             "src/win32_init.c",
+            "src/win32_module.c",
             "src/win32_joystick.c",
             "src/win32_monitor.c",
+            "src/win32_time.h",
             "src/win32_time.c",
+            "src/win32_thread.h",
             "src/win32_thread.c",
             "src/win32_window.c",
             "src/wgl_context.c",
--- SetUp.bat
+++ SetUp.bat
@@ -1,13 +1,22 @@
 @echo off
 REM GLFW premake5
+setlocal enabledelayedexpansion
+
 set glfw=GLFWpremake5.lua
 set glfwDestination=V8Engine\vendor\GLFW\premake5.lua
+
 if exist "%glfw%" (
     if not exist "%glfwDestination%" (
         copy "%glfw%" "%glfwDestination%"
         echo copy done: %glfwDestination%
     ) else (
-        echo premake5.lua for GLFW script is already exist
+        fc "%glfw%" "%glfwDestination%" > nul
+        if errorlevel 1 (
+            copy "%glfw%" "%glfwDestination%"
+            echo file updated: %glfwDestination%
+        ) else (
+            echo premake5.lua for GLFW script is already up to date
+        )
     )
 ) else (
     echo file is missing: %glfw%
