e5f80e6906c997a4b3569299aa0750b874fcb584
jeongmin94 <extraking@naver.com>
2024-08-28 17:31:28 +0900

    fix ImGui premake5

--- ImGuipremake5.lua
+++ ImGuipremake5.lua
@@ -8,15 +8,16 @@ project "ImGui"
 	files
 	{
 		"imconfig.h",
-		"imgui.h",
-		"imgui.cpp",
+		"imgui_demo.cpp",
 		"imgui_draw.cpp",
 		"imgui_internal.h",
+		"imgui_tables.cpp",
 		"imgui_widgets.cpp",
+		"imgui.cpp",
+		"imgui.h",
 		"imstb_rectpack.h",
 		"imstb_textedit.h",
 		"imstb_truetype.h",
-		"imgui_demo.cpp"
 	}
 
 	filter "system:windows"
