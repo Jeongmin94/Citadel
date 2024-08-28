53b9a7ee9d569058b383908bb6f342e7fc820412
jeongmin94 <extraking@naver.com>
2024-08-28 16:00:50 +0900

    Revert "Delete premake5.lua"
    This reverts commit d766ef213eb9201d22cd361f8a42a825bdb72b9c.

---
+++ premake5.lua
@@ -0,0 +1,141 @@
+workspace "Citadel"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}" 
+
+-- Include directories relative to root folder(solution directory)
+IncludeDir = {}
+IncludeDir["GLFW"] = "V8Engine/vendor/GLFW/include"
+IncludeDir["Glad"] = "V8Engine/vendor/Glad/include"
+IncludeDir["ImGui"] = "V8Engine/vendor/ImGui"
+
+-- include premake5.lua files in directory
+include "V8Engine/vendor/GLFW"
+include "V8Engine/vendor/Glad"
+include "V8Engine/vendor/ImGui"
+
+project "V8Engine"
+	location "V8Engine" 
+	kind "SharedLib"
+	language "C++"
+	staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	-- for pch use
+	pchheader "v8pch.h"
+	-- for pch create
+	pchsource "V8Engine/src/v8pch.cpp"
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp" 
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include",
+		"%{prj.name}/src",
+		"%{IncludeDir.GLFW}",
+		"%{IncludeDir.Glad}",
+		"%{IncludeDir.ImGui}",
+	}
+
+	links
+	{
+		"GLFW",			-- project GLFW
+		"Glad", 		-- project Glad
+		"ImGui",		-- project ImGui
+		"opengl32.lib"
+	}
+
+	filter "system:windows"
+		cppdialect "C++20"
+		systemversion "latest"
+
+		defines
+		{
+			"V8_PLATFORM_WINDOWS", 
+			"V8_BUILD_DLL",
+			"GLFW_INCLUDE_NONE",
+			"IMGUI_IMPL_OPENGL_LOADER_CUSTOM",
+		}
+
+	postbuildcommands
+	{
+		("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Citadel")
+	}
+
+	filter "configurations:Debug"
+		defines "CITADEL_DEBUG"
+		runtime "Debug"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "CITADEL_RELEASE"
+		runtime "Release"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "CITADEL_DIST"
+		runtime "Release"
+		optimize "On"
+
+project "Citadel"
+	location "Citadel"
+	kind "ConsoleApp"
+	language "C++"
+	staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"V8Engine/vendor/spdlog/include",
+		"V8Engine/src"
+	}
+
+	links
+	{
+		"V8Engine"
+	}
+
+	filter "system:windows"
+		cppdialect "C++20"
+		systemversion "latest"
+
+		defines
+		{
+			"V8_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "CITADEL_DEBUG"
+		runtime "Debug"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "CITADEL_RELEASE"
+		runtime "Release"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "CITADEL_DIST"
+		runtime "Release"
+		optimize "On"
